0fdee0c6d507fdf4240a489bdcb5999b2820539d
Felipe Brigatto <felipe.brigatto@wafx.global>
2020-12-12 02:13:31 -0300

    docstring script import db json

--- scripts/import_db_json.py
+++ scripts/import_db_json.py
@@ -1,3 +1,34 @@
+""" Script to import data given a database exported in JSON
+    
+    This is the structure:
+
+    [
+        {
+            "full_price": float,
+            "price_with_discount": float,
+            "discount_percentage": float,
+            "start_date": string,
+            "enrollment_semester": string,
+            "enabled": boolean,
+            "course": {
+                "name": string,
+                "kind": string,
+                "level": string,
+                "shift": string
+            },
+            "university": {
+                "name": string,
+                  "score": float,
+              "logo_url": string
+            },
+            "campus": {
+                "name": string,
+                "city": string
+            }
+        },
+    ...
+    ]
+"""
 import json
 from quero.offer.models import Offer, Campus, University, Course
 
@@ -9,19 +40,22 @@ with open('scripts/db.json') as file_data:
             name=offer['university']['name'],
             score=offer['university']['score'],
             logo_url=offer['university']['logo_url'],
-        )[0]  # because get_or_create() returns a tuple
+        )[0]  # because get_or_create() returns a tuple (obj, bool)
+
         campus = Campus.objects.get_or_create(
             name=offer['campus']['name'],
             city=offer['campus']['city'],
             university=university,
-        )[0]  # because get_or_create() returns a tuple
+        )[0]  # because get_or_create() returns a tuple (obj, bool)
+
         course = Course.objects.get_or_create(
             name=offer['course']['name'],
             kind=offer['course']['kind'],
             level=offer['course']['level'],
             shift=offer['course']['shift'],
             campus=campus,
-        )[0]  # because get_or_create() returns a tuple
+        )[0]  # because get_or_create() returns a tuple (obj, bool)
+
         Offer.objects.create(
             full_price=offer['full_price'],
             price_with_discount=offer['price_with_discount'],
