6c29114585bc2b97041550d1513ed676f38acce2
Felipe Brigatto <felipe.brigatto@wafx.global>
2020-12-13 19:39:33 -0300

    install django-filter

--- quero/settings.py
+++ quero/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'django_extensions'
 ]
 
@@ -56,6 +57,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+
+}
 
 ROOT_URLCONF = 'quero.urls'
 
