000a5b822904604119a356f60294f3ad28da730d
Felipe Brigatto <felipe.brigatto@wafx.global>
2020-12-13 21:40:49 -0300

    created test case to create an offer from nothing

--- quero/offer/tests.py
+++ quero/offer/tests.py
@@ -16,9 +16,9 @@ class UserTests(APITestCase):
         self.assertEqual(User.objects.first().username, "DabApps")
 
 
-class UniversityTests(APITestCase):
-    def test_create_account(self):
-        response = self.client.post(
+class QueroListarTests(APITestCase):
+    def test_create_university_campus_course_offer(self):
+        response_university = self.client.post(
             reverse("university-list"),
             {
                 "name": "Anhembi",
@@ -27,4 +27,43 @@ class UniversityTests(APITestCase):
             },
             format="json",
         )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_university.status_code, status.HTTP_201_CREATED)
+
+        response_campus = self.client.post(
+            reverse("campus-list"),
+            {
+                "name": "Name Of Campus",
+                "city": "São José dos Campos",
+                "university": response_university.data["id"],
+            },
+            format="json",
+        )
+        self.assertEqual(response_campus.status_code, status.HTTP_201_CREATED)
+
+        response_course = self.client.post(
+            reverse("course-list"),
+            {
+                "name": "Jornalismo",
+                "kind": "Presencial",
+                "level": "Bacharelado",
+                "shift": "Noite",
+                "campus": response_campus.data["id"],
+            },
+            format="json",
+        )
+        self.assertEqual(response_course.status_code, status.HTTP_201_CREATED)
+
+        response_offer = self.client.post(
+            reverse("offer-list"),
+            {
+                "full_price": 1227.05,
+                "price_with_discount": 515.36,
+                "discount_percentage": 58.01,
+                "start_date": "01/08/2019",
+                "enrollment_semester": "2019.2",
+                "enabled": True,
+                "course": response_course.data["id"],
+            },
+            format="json",
+        )
+        self.assertEqual(response_offer.status_code, status.HTTP_201_CREATED)
