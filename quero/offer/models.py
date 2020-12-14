ddb7c799853de75b7f168c86f6cca1c4d1963655
Felipe Brigatto <felipe.brigatto@wafx.global>
2020-12-13 22:07:23 -0300

    naming attributes on models

--- quero/offer/models.py
+++ quero/offer/models.py
@@ -8,9 +8,9 @@ from django.utils.translation import gettext_lazy as _
 class Offer(models.Model):
     """docstring for Offer"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    full_price = models.FloatField()
-    price_with_discount = models.FloatField()
-    discount_percentage = models.FloatField()
+    full_price = models.FloatField(_("Full Price"))
+    price_with_discount = models.FloatField(_("Price With Discount"))
+    discount_percentage = models.FloatField(_("Discount Percentage"))
     start_date = models.CharField(_("Stard Date"), max_length=10)
     enrollment_semester = models.CharField(_("Enrollment Semester"), max_length=6)
     enabled = models.BooleanField(default=False)
@@ -39,5 +39,5 @@ class University(models.Model):
     """docstring for University"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(_("Company"), max_length=255)
-    score = models.FloatField()
+    score = models.FloatField(_("Score"))
     logo_url = models.CharField(_("Logo"), max_length=255)
